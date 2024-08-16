f991fc098c2b60c17ca08a0e7dc8bcf72606cf58
Dar Levy <darlevy8@gmail.com>
2024-08-16 13:36:43 +0300

    Create InitializeBoard

--- Ex02_Othelo/Ex02_Othelo/Board.cs
+++ Ex02_Othelo/Ex02_Othelo/Board.cs
@@ -1,19 +1,32 @@
+
 namespace Ex02_Othelo;
 
 public class Board
 {
-    private int m_Size;
+    private readonly int m_Width;
+    private readonly int m_Height;
     private char[,] m_Grid;
 
-    public Board(int i_Size)
+    public Board(int i_Width, int i_Height)
     {
-        m_Size = i_Size;
-        m_Grid = new char[m_Size, m_Size];
+        m_Width = i_Width;
+        m_Height = i_Height;
+        m_Grid = new char[m_Width, m_Height];
+        InitializeBoard();
+    }
+    
+    private void InitializeBoard()
+    {
+        int middleWidthIndex = (int)Math.Floor((decimal)(m_Width / 2));
+        int middleHeightIndex = (int)Math.Floor((decimal)(m_Height / 2));
         
-        // player1 = new Player()
+        setCell(eColor.White, middleWidthIndex - 1, middleHeightIndex - 1);
+        setCell(eColor.White, middleWidthIndex, middleHeightIndex);
+        setCell(eColor.Black, middleWidthIndex, middleHeightIndex - 1);
+        setCell(eColor.Black, middleWidthIndex - 1, middleHeightIndex);
     }
 
-    private void InitializeBoard()
+    public void setCell(eColor color, int x, int y)
     {
         
     }
