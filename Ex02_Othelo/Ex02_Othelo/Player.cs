92abfe18d3c0794f2d60f56629e6985e9b2fb1a0
Dar Levy <darlevy8@gmail.com>
2024-08-16 12:24:27 +0300

    Create getters and setters

--- Ex02_Othelo/Ex02_Othelo/Player.cs
+++ Ex02_Othelo/Ex02_Othelo/Player.cs
@@ -6,7 +6,33 @@ public class Player
     private string m_name;
     private int m_score;
 
-
+    public string Color
+    {
+        get
+        {
+            return m_color;
+        }
+    }
+    
+    public string Name
+    {
+        get
+        {
+            return m_name;
+        }
+    }
+    
+    public int Score
+    {
+        get
+        {
+            return m_score;
+        }
+        set
+        {
+            m_score = value;
+        }
+    }
 
     public Player(string i_name, int i_score, string i_color)
     {
