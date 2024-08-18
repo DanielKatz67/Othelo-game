5b80b69c59c4b51e27b840056d100c396927808b
Dar Levy <darlevy8@gmail.com>
2024-08-18 22:01:59 +0300

    Rename OtheloGame properties

--- Ex02_Othelo/Ex02_Othelo/OtheloGame.cs
+++ Ex02_Othelo/Ex02_Othelo/OtheloGame.cs
@@ -4,16 +4,17 @@ namespace Ex02_Othelo;
 
 public static class OtheloGame
 {
-    private static Player player1;
-    private static Player player2;
+    private static Player m_Player1;
+    private static Player m_Player2;
+    private static Player m_CurrentPlayer;
     
     public static void Run()
     {
         Console.WriteLine("Welcome to Othelo Game!");
-        player1 = getPlayer("Enter your name: ", eColor.Black);
-        player2 = getPlayer("Opponent! enter your name: ", eColor.White);
+        m_Player1 = getPlayer("Enter your name: ", eColor.Black);
+        m_Player2 = getPlayer("Opponent! enter your name: ", eColor.White);
     }
-
+    
     private static Player getPlayer(string i_Message,eColor i_Color)
     {
         Console.WriteLine(i_Message);
