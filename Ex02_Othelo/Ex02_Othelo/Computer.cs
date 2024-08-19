5c1ef44265e27c96e34eebb76ce67c45dbbbff0a
Dar Levy <darlevy8@gmail.com>
2024-08-19 11:36:36 +0300

    Create MoveRandomly method and extract some code to getValidMoves

--- Ex02_Othelo/Ex02_Othelo/Computer.cs
+++ Ex02_Othelo/Ex02_Othelo/Computer.cs
@@ -31,8 +31,34 @@ public class Computer
         m_Color = i_Color;
     }
 
-    public void ImplementRandomMove()
+    public void MoveRandomly(Board i_Board)
     {
-        throw new NotImplementedException();
+        List<Coordinate> validMoves = getValidMoves(i_Board);
+
+        if (validMoves.Count > 0)
+        {
+            Random random = new Random();
+            Coordinate selectedMove = validMoves[random.Next(validMoves.Count)];
+            i_Board.SetCell(m_Color, selectedMove);
+        }
+    }
+
+    private List<Coordinate> getValidMoves(Board i_Board)
+    {
+        List<Coordinate> validMoves = new List<Coordinate>();
+
+        for (int x = 0; x < i_Board.Width; x++)
+        {
+            for (int y = 0; y < i_Board.Height; y++)
+            {
+                Coordinate potentialMove = new Coordinate(x, y);
+                if (BoardValidator.CellIsValid(potentialMove, m_Color, BoardValidator.IdentifyAllEdges(potentialMove, m_Color, i_Board), i_Board))
+                {
+                    validMoves.Add(potentialMove);
+                }
+            }
+        }
+
+        return validMoves;
     }
 }
